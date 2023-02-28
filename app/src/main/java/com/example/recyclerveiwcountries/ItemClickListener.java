7c2277c030b067ff843fe78e18263bc1be3c7aed
KMS <kms125690@gmail.com>
2023-02-28 19:55:35 +0800

    add handler for item clicks in RecyclerView

---
+++ app/src/main/java/com/example/recyclerveiwcountries/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.recyclerveiwcountries;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onItemClick(View v, int pos);
+}
--- app/src/main/java/com/example/recyclerveiwcountries/MyAdapter.java
+++ app/src/main/java/com/example/recyclerveiwcountries/MyAdapter.java
@@ -1,6 +1,9 @@
 package com.example.recyclerveiwcountries;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import android.view.animation.AnimationUtils;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyHolder> {
@@ -36,6 +40,31 @@ public class MyAdapter extends RecyclerView.Adapter<MyHolder> {
         holder.mCountryName.setText(models.get(position).getCountryName());
         holder.mCountryArea.setText("The total Area :" + models.get(position).getCountryArea());
         holder.mImageFlag.setImageResource(models.get(position).getImage());
+
+        // Use when you want to put each item data to same activity
+        holder.mRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get data from item click
+                String countryName = models.get(position).getCountryName();
+                String countryArea = models.get(position).getCountryArea();
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) holder.mImageFlag.getDrawable();
+                // get bitmap from drawable
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                // compress image
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                //convert to bytes array
+                byte[] bytes = stream.toByteArray();
+                // intent, put data to intent, start activity
+                Intent intent = new Intent(c, DetailActivity.class);
+                intent.putExtra("cName", countryName);
+                intent.putExtra("cArea", countryArea);
+                intent.putExtra("cImage", bytes);
+                c.startActivity(intent);
+            }
+        });
+
         // Animation
         Animation animation = AnimationUtils.loadAnimation(c, android.R.anim.slide_in_left);
         holder.itemView.startAnimation(animation);
